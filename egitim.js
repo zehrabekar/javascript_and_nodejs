45bd617311f86ab00e73a2f52a89253708461b89
Zehra Bekar <zehrabekar54@gmail.com>
2023-02-21 17:42:31 +0300

    variable creation practices

--- egitim.js
+++ egitim.js
@@ -1,1 +1,38 @@
+/*1.bölüm*/
 console.log("Merhaba from egitim.js")
+
+/* 2.bölüm*/
+var plaka = 54
+console.log(plaka)
+console.log(plaka)
+console.log(plaka)
+console.log(plaka)
+console.log(plaka)
+
+let _sayi = 123
+let $sayi =456
+let sayi11 = 458
+let benim_kimlik_numaram = 123456
+
+// bu şekilde sayi2'ye değer vermeden yazdırma işlemi yaptırdığımızda undefined olarak göründü
+console.log(sayi2);
+//şimdi sayi2 ile ekranda 50 değeri göründü
+sayi2=50;
+console.log(sayi2);
+// sayi2'ye iki değer verdim, ekranda verilen son değer göründü
+sayi2=60;
+sayi2=70;
+console.log(sayi2);
+
+// const ile değişken tanımladığımda o değişkene verdiğimiz değer sabit kalır ve sayi2de yaptığım gibi sonradan eklediğim değer kabul edilmez
+/*const piSayisi = 3.14
+piSayisi=3.15
+console.log(piSayisi)
+sabit değişkene tekrar değer atama işlemi yapmaya çalıştığım için hata verdi*/
+
+const piSayisi = 3.14
+console.log(piSayisi)
+
+// bir değişken oluştururken let veya const kullanmadan da oluşturulur fakat doğru değilir kullanılmaması gerekir
+notum = 100;
+console.log(notum)
