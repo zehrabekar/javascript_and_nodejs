516db02630bb16dec39a04bf7cb3f00170961361
Zehra Bekar <zehrabekar54@gmail.com>
2023-03-01 13:43:47 +0300

    ternary if usage

--- egitim2.js
+++ egitim2.js
@@ -43,9 +43,30 @@ let rakam = 5, rakam1 = 6
     else {
         console.log("else çalıştı")
     }
-
+/*
 let asalSayiMi = true
-// if (asalSayiMi == true) = if (asalSayiMi)
+ if (asalSayiMi == true) = if (asalSayiMi) 
+ asalSayiMi değişkenine true değerini verdikten sonra tekrar  if (asalSayiMi == true) yazmamıza gerek yoktur. onun yerine direkt if (asalSayiMi) yazdığımızda zaten true 
+ algılanır*/
+
+ // (ternary  kısa if kullanımı)
+ 
+ let a = 10, b=5, c=0;
+ if (a > b) {
+    c = a+b
+ }
+else {
+    c = a-b
+}
+console.log("c sayısı :"+c)
+
+// yukarıdaki if işleminin kısa yolu : 
+c = (a > b) ? (a + b) : (a - b)
+console.log("c'nin yeni değeri :" +c)
+
+/* buradaki ?'nin anlamı : "eğer a b'en büyük ise" cümlesindeki ise'nin karşılığıdır
+: 'nın anlamı ise else'e karşılık gelir. */
+
 
 
 
