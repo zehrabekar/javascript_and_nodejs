a6366f3511e841e323f524f4a38aa5f198bb9add
Zehra Bekar <zehrabekar54@gmail.com>
2023-03-07 20:27:43 +0300

    new solution added

--- egitim4.js
+++ egitim4.js
@@ -20,17 +20,29 @@ if((kenarBir===kenarIki)&& (kenarIki===kenarUc)){
 } else {
     alert("bu çeşitkenar bir üçgendir")
 }
-*/
+2.yol
+let kenarBir = parseInt(prompt("birinci kenarı giriniz","5"))
+let kenarIki = parseInt(prompt("ikinci kenarı giriniz","5"))
+let kenarUc = parseInt(prompt("ucuncu kenarı giriniz","5"))
+
+if((kenarBir===kenarIki)&& (kenarIki===kenarUc)){ 
+    alert("bu eşkenar bir üçgendir")
+}else if (kenarBir !== kenarIki && kenarBir !== kenarUc && kenarIki !== kenarUc){
+    alert("bu çeşitkenar bir üçgendir")
+} else {
+    alert("bu ikizkenar bir üçgendir")
+}
+*/ 
 
 // s-3 :klavyeden girilen vize ve final notlarına göre öğrencinin dersi geçip geçmediğini söyleyen bir uygulama (geçme notu 50. vizenin %40ı , finalin %60ı geçerlidir)
-let vize = parseInt(prompt("vize notunu giriniz","100"))
+/*let vize = parseInt(prompt("vize notunu giriniz","100"))
 let final = parseInt(prompt("final notunu giriniz","100"))
 let puan = ((vize * 40/100) + (final * 60/100))
 if(puan < 50){
     alert("maalesef sınıfta kaldınız : "+ puan)
 }else {
     alert("tebrikler, sınıfı geçtiniz. notunuz :" + puan)
-}
+}*/
 
 
 
