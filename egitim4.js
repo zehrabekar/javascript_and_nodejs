c99d822ae7101afe4086f9bea389b63eca109f78
Zehra Bekar <zehrabekar54@gmail.com>
2023-03-08 17:36:06 +0300

    new solution added for third question

--- egitim4.js
+++ egitim4.js
@@ -1,13 +1,18 @@
 //sorular
+
 //s-1 : girilen 3 sayısının ortalamsını hesapla
 /*
 let sayiBir = parseInt(prompt("birinci sayıyı giriniz"))
 let sayiIki = parseInt(prompt("ikinci sayıyı giriniz"))
 let sayiUc = parseInt(prompt("üçüncü sayıyı giriniz"))
 let ortalama = ((sayiBir + sayiIki + sayiUc)/3)
-alert("ortalama :" + ortalama)*/
+alert("ortalama :" + ortalama)
+
+-----------------------------------------------------------------------------*/
 
 //s-2 : klavyeden kenarları girilen bir üçgenin çeşidini veren uygulama
+
+//benim çözümüm 
 /*
 let kenarBir = parseInt(prompt("birinci kenarı giriniz","5"))
 let kenarIki = parseInt(prompt("ikinci kenarı giriniz","5"))
@@ -20,6 +25,7 @@ if((kenarBir===kenarIki)&& (kenarIki===kenarUc)){
 } else {
     alert("bu çeşitkenar bir üçgendir")
 }
+
 2.yol
 let kenarBir = parseInt(prompt("birinci kenarı giriniz","5"))
 let kenarIki = parseInt(prompt("ikinci kenarı giriniz","5"))
@@ -32,17 +38,51 @@ if((kenarBir===kenarIki)&& (kenarIki===kenarUc)){
 } else {
     alert("bu ikizkenar bir üçgendir")
 }
-*/ 
 
-// s-3 :klavyeden girilen vize ve final notlarına göre öğrencinin dersi geçip geçmediğini söyleyen bir uygulama (geçme notu 50. vizenin %40ı , finalin %60ı geçerlidir)
-/*let vize = parseInt(prompt("vize notunu giriniz","100"))
+-----------------------------------------------------------------------------*/
+
+/* s-3 :klavyeden girilen vize ve final notlarına göre öğrencinin dersi geçip geçmediğini söyleyen bir uygulama (geçme notu 50. vizenin %40ı , finalin %60ı geçerlidir)
+
+let enteredValue
+let vize
+let final
+let totalPoint
+let propmtMessage
+
+while (vize == undefined || final == undefined) {
+    
+    if(vize == undefined) {
+        propmtMessage = "Vize notunuzu 0 - 100 arasında giriniz"
+    } else {
+        propmtMessage = "Final notunuzu 0 - 100 arasında giriniz"
+    }
+
+    enteredValue = parseInt(prompt(propmtMessage,"100"))
+    if(enteredValue < 0 || enteredValue > 100) {
+        continue
+    }
+
+    if(vize == undefined) {
+        vize = enteredValue
+    } else {
+        final = enteredValue
+    }
+}
+
+totalPoint = ((vize * 40/100) + (final * 60/100))
+alert(`Ortalamanız ${totalPoint}`) 
+*/
+
+
+/*
+ let vize = parseInt(prompt("vize notunu giriniz","100"))
 let final = parseInt(prompt("final notunu giriniz","100"))
-let puan = ((vize * 40/100) + (final * 60/100))
-if(puan < 50){
-    alert("maalesef sınıfta kaldınız : "+ puan)
-}else {
-    alert("tebrikler, sınıfı geçtiniz. notunuz :" + puan)
-}*/
+ let puan = ((vize * 40/100) + (final * 60/100))
+ if(puan < 50){
+     alert("maalesef sınıfta kaldınız : "+ puan)
+ }else {
+     alert("tebrikler, sınıfı geçtiniz. notunuz :" + puan)
+ }*/
 
 
 
