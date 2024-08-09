56fb6e606973b36fb0cefa65daaaa4e16d353b5e
Zehra Bekar <zehrabekar54@gmail.com>
2024-08-09 16:21:16 +0300

    added notes about Event Listeners and Event Object

--- egitim23.js
+++ egitim23.js
@@ -1,5 +1,8 @@
 //Event Listeners ve Event Objesi Kullanma
 
+//Event Listeners : bir olayın gerçekleşmesini bekleyen ve bu olay gerçekleştiğinde belirli bir işlevi (fonksiyonu) çalıştıran mekanizmalardır. 
+//Olaylar, bir kullanıcı tıklaması, klavyede bir tuşa basılması, fare hareketi, sayfa yüklemesi gibi şeyler olabilir.
+
 //DOM Eventleri web site üzerinde mouse, klavye gibi kullanıcı etkileşimiyle ortaya çıkan olaylardır. Bu olaylar, Javascript ile belirli olaylar 
 //gerçekleştiğinde gerekli kodların çalışmasını sağlayan etkileşimli web sayfaları oluşturmak için kullanılır.
 
@@ -20,11 +23,15 @@ const filterInput = document.getElementById("filter"); // <input class="form-con
 
 filterInput.addEventListener("focus",function(e){
     console.log(e); //FocusEvent {isTrusted: true, relatedTarget: null, view: Window, detail: 0, sourceCapabilities: InputDeviceCapabilities, …}
-    console.log(e.type); //focus
-    console.log(e.target); //<input class="form-control" type="text" name="filter" id="filter" placeholder="Bir Todo Arayın">
-   console.log(e.target.placeholder); //Bir Todo Arayın
-   console.log(e.target.className); //form-control
+    console.log(e.type); //focus (olay tipi)
+    console.log(e.target); //<input class="form-control" type="text" name="filter" id="filter" placeholder="Bir Todo Arayın">  (olayın meydana geldiği HTML elementi)
+   console.log(e.target.placeholder); //Bir Todo Arayın (olayın meydana geldiği HTML elementinin placeholder değeri)
+   console.log(e.target.className); //form-control (olayın meydana geldiği HTML elementinin className'i)
 });
+
+//function(e), e event objesidir. e, "focus" olayı tetiklendiğinde tarayıcı tarafından otomatik olarak oluşturulan bir nesnedir.
+//e, fonksiyonun içinde bir parametre olarak kullanılır ve bu sayede olayla ilgili bilgilere erişebiliriz.
+
 
 //submit event
 const todoForm = document.getElementById("todo-form");
